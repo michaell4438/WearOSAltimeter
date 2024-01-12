74a77a99f9681b00805e5e7cf7ef4af153223a70
Michael Lachut <michael@lachut.com>
2024-01-11 21:06:42 -0500

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.michaell.wearos_altimeter"
         minSdk = 30
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
         vectorDrawables {
             useSupportLibrary = true
         }
