13ba6ff54883786055b38552829ff4fa3043418e
Michael Lachut <michael@lachut.com>
2024-01-11 21:06:21 -0500

    Disabled Debugging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            isDebuggable = false
         }
     }
     compileOptions {
